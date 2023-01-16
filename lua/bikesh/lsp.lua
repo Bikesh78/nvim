9d818b6558fedd204646bc30269a1ccdf30dbb7b
Bikesh Rai <bikeshrai78@gmail.com>
2023-01-16 21:02:39 +0545

    Add code autocompletion

--- lua/bikesh/lsp.lua
+++ lua/bikesh/lsp.lua
@@ -15,10 +15,11 @@ require("mason-lspconfig").setup({
   automatic_installation = true
 })
 
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- Set up neovim-lspconfig
 
--- Mappings.
+-- Mappings. 
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 
@@ -46,7 +47,7 @@ local on_attach = function(client, bufnr)
   vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
   vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+--  vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
   vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
   vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
   vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
@@ -55,6 +56,8 @@ end
 -- loop through servers and add each language servers
 for _, server in ipairs(servers) do
   require('lspconfig')[server].setup {
-    on_attach = on_attach
+    on_attach = on_attach,
+    capabilities = capabilities
   }
 end
+
--- lua/bikesh/plugins.lua
+++ lua/bikesh/plugins.lua
@@ -1,34 +1,39 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
   -- Packer manager
   use 'wbthomason/packer.nvim'
 
   -- telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
 
-use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
+  use 'nvim-lualine/lualine.nvim'
 
-use 'nvim-lualine/lualine.nvim'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
 
-use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate'
-    }
+  -- lsp
+  use "williamboman/mason.nvim"
+  use "williamboman/mason-lspconfig.nvim"
+  use "neovim/nvim-lspconfig"
 
-use "williamboman/mason.nvim" 
-use "williamboman/mason-lspconfig.nvim"
-use "neovim/nvim-lspconfig"
---use "hrsh7th/nvim-cmp"
+  -- autocomplete
+  use "hrsh7th/nvim-cmp"
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
 
-use "kyazdani42/nvim-web-devicons"
-use 'nvim-tree/nvim-tree.lua'
+  -- snippet
+  use "L3MON4D3/LuaSnip"
 
--- tokyo night colour scheme
-use 'folke/tokyonight.nvim'
+  --file explorer
+  use "kyazdani42/nvim-web-devicons"
+  use 'nvim-tree/nvim-tree.lua'
+
+  -- tokyo night colour scheme
+  use 'folke/tokyonight.nvim'
 end)
+
